0f33f3bbc75440765e0c020e8d03475f0d53a260
Yogi <Yogi@172.19.98.253>
2015-09-21 11:22:44 +0100

    back end in palce to get the attendees for an event -- can not register them yet

---
+++ eGym/src/main/java/Stores/BasicUser.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Stores;
+
+/**
+ *
+ * @author Yogi
+ */
+
+//Basic user to be used for signups where we need only the very basic details
+
+
+public class BasicUser {
+    
+    private String Username = null;
+    private String FirstName  = null;
+    private String LastName = null;
+    private String Email = null;
+    private String Phone = null;
+
+    public BasicUser(){} //empty constructer -- saves initialising something as nil
+    
+    /**
+     * @return the Username
+     */
+    public String getUsername() {
+        return Username;
+    }
+
+    /**
+     * @param Username the Username to set
+     */
+    public void setUsername(String Username) {
+        this.Username = Username;
+    }
+
+    /**
+     * @return the FirstName
+     */
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    /**
+     * @param FirstName the FirstName to set
+     */
+    public void setFirstName(String FirstName) {
+        this.FirstName = FirstName;
+    }
+
+    /**
+     * @return the LastName
+     */
+    public String getLastName() {
+        return LastName;
+    }
+
+    /**
+     * @param LastName the LastName to set
+     */
+    public void setLastName(String LastName) {
+        this.LastName = LastName;
+    }
+
+    /**
+     * @return the Email
+     */
+    public String getEmail() {
+        return Email;
+    }
+
+    /**
+     * @param Email the Email to set
+     */
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    /**
+     * @return the Phone
+     */
+    public String getPhone() {
+        return Phone;
+    }
+
+    /**
+     * @param Phone the Phone to set
+     */
+    public void setPhone(String Phone) {
+        this.Phone = Phone;
+    }
+    
+}
---
+++ eGym/src/main/webapp/EventAttendance.jsp
@@ -0,0 +1,22 @@
+<%-- 
+    Document   : EventAttendance
+    Created on : Sep 21, 2015, 10:33:27 AM
+    Author     : Yogi
+--%>
+
+<%@page import="java.util.ArrayList"%>
+<%@page import="java.util.LinkedList"%>
+<%@page import="java.util.Iterator"%>
+<%@page import="Stores.BasicUser"%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
