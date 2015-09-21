18bc93b1a8d44eca69a042a56e1e28e9159bf6fe
txroberts <t.x.roberts@dundee.ac.uk>
2015-09-21 16:47:38 +0100

    Password reset
    Generates a new salt

---
+++ eGym/src/main/java/com/egym/ResetPassword.java
@@ -0,0 +1,135 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.egym;
+
+import Lib.AeSimpleSHA256;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Tom
+ */
+@WebServlet(name = "ResetPassword", urlPatterns = {"/ResetPassword"})
+public class ResetPassword extends HttpServlet {
+    
+    Connection con = null;
+    Statement st = null;
+    ResultSet rs = null;
+    static final String JDBC_DRIVER ="com.mysql.jdbc.Driver";  
+    String url = "jdbc:mysql://46.101.32.81:3306/EGAlexander";
+    String user = "root";
+    String password = "teameight";
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.sendRedirect("resetPassword.jsp");
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, UnsupportedEncodingException {
+        String username = request.getParameter("username");
+        String newPassword = request.getParameter("password");
+        
+        SecureRandom random = new SecureRandom();
+        byte[] salt = new byte[32];
+        random.nextBytes(salt);
+
+        String encodedPassword = null;            
+        String hexSalt = null;
+        try {
+            encodedPassword = AeSimpleSHA256.getHash(newPassword, salt);
+            hexSalt = AeSimpleSHA256.toHex(salt);
+        }
+        catch (NoSuchAlgorithmException et) {
+            System.out.println("Can't hash the password");
+            response.sendRedirect("index.html");
+        } catch (InvalidKeySpecException ex) {
+            Logger.getLogger(ResetPassword.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            con = DriverManager.getConnection(url, user, password);
+            CallableStatement cs = null;
+            cs = this.con.prepareCall("{call update_user_password(?, ?, ?)}"); 
+            cs.setString(1, username);
+            cs.setString(2, encodedPassword);
+            cs.setString(3, hexSalt);
+            cs.executeQuery();
+            
+            cs.close();
+            con.close();
+            
+            RequestDispatcher rd = request.getRequestDispatcher("/index.html");
+            rd.forward(request,response);
+        } catch (SQLException | ClassNotFoundException | InstantiationException | IllegalAccessException ex) {
+            Logger.getLogger(ResetPassword.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ eGym/src/main/webapp/resetPassword.jsp
@@ -0,0 +1,25 @@
+<%-- 
+    Document   : resetPassword
+    Created on : 21-Sep-2015, 16:37:38
+    Author     : Tom
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Reset password</title>
+    </head>
+    <body>
+        <form class="form-signin" role="form" method="POST" action="ResetPassword">
+            <h2 class="form-signin-heading">Please enter new password</h2>
+            <label for="username" class="sr-only">Username</label>
+            <input type="text" name="username" class="form-control" placeholder="Username" required autofocus>
+            <label for="password" class="sr-only">Password</label>
+            <input type="password" name="password" class="form-control" placeholder="Password" required>
+            <br>
+            <button class="btn btn-lg btn-primary btn-block" type="submit">Reset password</button>
+        </form>
+    </body>
+</html>
