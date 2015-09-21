c9d0e33eb56908645a765fc168b7976ca9fcdc4d
Yogi <Yogi@172.19.98.253>
2015-09-21 14:26:23 +0100

    servlet (registerattendee/name/eventid) will now mark a user as attended that event..

--- eGym/src/main/java/com/egym/RegisterAttendee.java
+++ eGym/src/main/java/com/egym/RegisterAttendee.java
@@ -5,13 +5,25 @@
  */
 package com.egym;
 
+import Models.EventsModel;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.LinkedList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import Lib.Convertors;
 
 /**
  *
@@ -19,7 +31,13 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet(name = "RegisterAttendee", urlPatterns = {"/RegisterAttendee/*"})
 public class RegisterAttendee extends HttpServlet {
-
+    Connection con = null;
+    Statement st = null;
+    ResultSet rs = null;
+    static final String JDBC_DRIVER ="com.mysql.jdbc.Driver";  
+    String url = "jdbc:mysql://46.101.32.81:3306/EGAlexander";
+    String user = "root";
+    String password = "teameight";
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
@@ -30,9 +48,26 @@ public class RegisterAttendee extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+            throws ServletException, IOException, ClassNotFoundException, SQLException, InstantiationException, IllegalAccessException {
         response.setContentType("text/html;charset=UTF-8");
+        
+        
+        
+        
         try (PrintWriter out = response.getWriter()) {
+            
+             Class.forName("com.mysql.jdbc.Driver").newInstance();
+             String args[] = Convertors.SplitRequestPath(request);
+            con = DriverManager.getConnection(url, user, password);
+            CallableStatement cs = null;
+            cs = this.con.prepareCall("{call mark_activity_attendance(?,?)}");   //(?,?)}"
+            cs.setString(1, args[2]);
+            cs.setInt(2, Integer.parseInt(args[3]));
+            ResultSet rs = cs.executeQuery();
+            cs.close();
+                    con.close();
+           
+            //2 username 3 activity id
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
@@ -40,7 +75,8 @@ public class RegisterAttendee extends HttpServlet {
             out.println("<title>Servlet RegisterAttendee</title>");            
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet RegisterAttendee at " + request.getContextPath() + "</h1>");
+            out.println("<h1>"+args[2]+"</h1>");
+            out.println("<h1>"+args[3]+"</h1>");
             out.println("</body>");
             out.println("</html>");
         }
@@ -58,7 +94,17 @@ public class RegisterAttendee extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            processRequest(request, response);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(RegisterAttendee.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(RegisterAttendee.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            Logger.getLogger(RegisterAttendee.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(RegisterAttendee.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
@@ -72,7 +118,17 @@ public class RegisterAttendee extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            processRequest(request, response);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(RegisterAttendee.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(RegisterAttendee.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            Logger.getLogger(RegisterAttendee.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(RegisterAttendee.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
