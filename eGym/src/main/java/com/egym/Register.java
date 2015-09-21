b8f2cd090a2bc6b3ac1d68ecf4a938bbaaae076a
txroberts <t.x.roberts@dundee.ac.uk>
2015-09-21 12:30:58 +0100

    Hashes the password when registering

--- eGym/src/main/java/com/egym/Register.java
+++ eGym/src/main/java/com/egym/Register.java
@@ -5,8 +5,11 @@
  */
 package com.egym;
 
+import Lib.AeSimpleSHA256;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.security.NoSuchAlgorithmException;
 import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -48,12 +51,48 @@ public class Register extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            
+        
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.sendRedirect("register.jsp");
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
             String username = request.getParameter("username");
+            
             String userpassword = request.getParameter("password");
+            String encodedPassword = null;
+            try {
+                encodedPassword = AeSimpleSHA256.SHA256(userpassword);
+            }
+            catch (UnsupportedEncodingException | NoSuchAlgorithmException et) {
+                System.out.println("Can't hash the password");
+                response.sendRedirect("index.html");
+            }
+            
             String firstname = request.getParameter("firstname");
             String lastname = request.getParameter("lastname");
             String matriculation = request.getParameter("matricNO");
@@ -73,12 +112,10 @@ public class Register extends HttpServlet {
             
             Class.forName("com.mysql.jdbc.Driver").newInstance();
             con = DriverManager.getConnection(url, user, password);
-            CallableStatement cs = null;
-            CallableStatement cs2 = null;
-            cs = this.con.prepareCall("{call register_user(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)}"); 
-            cs2 = this.con.prepareCall("{call create_user_points(?)}");
+            
+            CallableStatement cs = this.con.prepareCall("{call register_user(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)}");
             cs.setString(1, username);
-            cs.setString(2, userpassword);
+            cs.setString(2, encodedPassword);
             cs.setString(3, firstname);
             cs.setString(4, lastname);
             cs.setString(5, matriculation);
@@ -94,50 +131,19 @@ public class Register extends HttpServlet {
             cs.setInt(15,1);
             cs.executeQuery();
             cs.close();
+            
+            CallableStatement cs2 = this.con.prepareCall("{call create_user_points(?)}");
             cs2.setString(1, username);
             cs2.executeQuery();
             cs2.close();
+            
             con.close();
+            
             RequestDispatcher rd = request.getRequestDispatcher("/index.html");
             rd.forward(request,response);
-        } catch (ClassNotFoundException ex) {
-            Logger.getLogger(Register.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (SQLException ex) {
-            Logger.getLogger(Register.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            Logger.getLogger(Register.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
+        } catch (ClassNotFoundException | SQLException | InstantiationException | IllegalAccessException ex) {
             Logger.getLogger(Register.class.getName()).log(Level.SEVERE, null, ex);
         }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
     }
 
     /**
